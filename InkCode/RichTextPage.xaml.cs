4e7bca6dbfca1e2fb3c8b38144c4883ac9639e1e
jpb <jakub_baranski@outlook.com>
2025-10-19 21:15:24 +0100

    RTF: Implement text styling functionality

--- InkCode/RichTextPage.xaml.cs
+++ InkCode/RichTextPage.xaml.cs
@@ -1,3 +1,4 @@
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.UI.Text;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
@@ -12,12 +13,11 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.System;
-using Windows.UI.Input.Preview.Injection;
-using Windows.UI.Text;
 using WinUIEditor;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -28,6 +28,7 @@ namespace InkCode
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+
     public sealed partial class RichTextPage : Page
     {
         public RichTextPage()
@@ -69,6 +70,50 @@ namespace InkCode
         private void Cut(object sender, RoutedEventArgs e)
         {
             editor.Document.Selection.Cut();
+        }
+
+        private void Bold(object sender, RoutedEventArgs e)
+        {
+            editor.Document.Selection.CharacterFormat.Bold = Microsoft.UI.Text.FormatEffect.Toggle;
+        }
+        private void Italic(object sender, RoutedEventArgs e)
+        {
+            editor.Document.Selection.CharacterFormat.Italic = Microsoft.UI.Text.FormatEffect.Toggle;
+        }
+        private void Underline(object sender, RoutedEventArgs e)
+        {
+            editor.Document.Selection.CharacterFormat.Underline = Microsoft.UI.Text.UnderlineType.Single;
+        }
+        private void Strikethrough(object sender, RoutedEventArgs e)
+        {
+            editor.Document.Selection.CharacterFormat.Strikethrough = Microsoft.UI.Text.FormatEffect.Toggle;
+        }
+
+        private void OnKeyboardAcceleratorInvoked(Microsoft.UI.Xaml.Input.KeyboardAccelerator sender, Microsoft.UI.Xaml.Input.KeyboardAcceleratorInvokedEventArgs args)
+        {
+            switch (sender.Key)
+            {
+                case Windows.System.VirtualKey.B:
+                    editor.Document.Selection.CharacterFormat.Bold = Microsoft.UI.Text.FormatEffect.Toggle;
+                    BoldButton.IsChecked = editor.Document.Selection.CharacterFormat.Bold == FormatEffect.On;
+                    args.Handled = true;
+                    break;
+                case Windows.System.VirtualKey.I:
+                    editor.Document.Selection.CharacterFormat.Italic = FormatEffect.Toggle;
+                    ItalicButton.IsChecked = editor.Document.Selection.CharacterFormat.Italic == FormatEffect.On;
+                    args.Handled = true;
+                    break;
+                case Windows.System.VirtualKey.U:
+                    editor.Document.Selection.CharacterFormat.Underline = UnderlineType.Single;
+                    UnderlineButton.IsChecked = editor.Document.Selection.CharacterFormat.Underline == UnderlineType.Single;
+                    args.Handled = true;
+                    break;
+                case Windows.System.VirtualKey.T:
+                    editor.Document.Selection.CharacterFormat.Strikethrough = FormatEffect.Toggle;
+                    StrikethroughButton.IsChecked = editor.Document.Selection.CharacterFormat.Strikethrough == FormatEffect.On;
+                    args.Handled = true;
+                    break;
+            }
         }
     }
 }
