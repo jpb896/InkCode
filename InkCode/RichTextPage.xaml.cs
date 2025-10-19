bb863677d9bf3713cf6dbc63c4ed36e169a0fc22
jpb <jakub_baranski@outlook.com>
2025-10-19 17:10:25 +0100

    RTF: Implement clipboard functionality

--- InkCode/RichTextPage.xaml.cs
+++ InkCode/RichTextPage.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Text;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -7,11 +8,17 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Input.Preview.Injection;
+using Windows.UI.Text;
+using WinUIEditor;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +33,42 @@ namespace InkCode
         public RichTextPage()
         {
             InitializeComponent();
+        }
+
+        private void Paste(SplitButton sender, SplitButtonClickEventArgs args)
+        {
+            editor.Document.Selection.Paste(0);
+        }
+
+        // This method is currently WIP. It does not work properly yet.
+        private async void PasteNoFormat(object sender, RoutedEventArgs e)
+        {
+            string text;
+            DataPackageView dataPackageView = Clipboard.GetContent();
+            editor.Document.GetText(Microsoft.UI.Text.TextGetOptions.FormatRtf, out text);
+            if (dataPackageView.Contains(StandardDataFormats.Text)) 
+            {
+                editor.Document.SetText(Microsoft.UI.Text.TextSetOptions.FormatRtf, text + dataPackageView.GetTextAsync());
+            } 
+            else
+            {
+                Debug.WriteLine("ERROR: Not pasting text");
+            }
+        }
+
+        private void Paste2(object sender, RoutedEventArgs e)
+        {
+            editor.Document.Selection.Paste(0);
+        }
+
+        private void Copy(object sender, RoutedEventArgs e)
+        {
+            editor.Document.Selection.Copy();
+        }
+
+        private void Cut(object sender, RoutedEventArgs e)
+        {
+            editor.Document.Selection.Cut();
         }
     }
 }
