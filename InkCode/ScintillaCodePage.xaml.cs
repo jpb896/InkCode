2e2904fa3be7f4f7db26d0dfcb26bf81e26404f3
jpb <jakub_baranski@outlook.com>
2025-10-21 09:09:06 +0100

    Code: Add Ribbon

--- InkCode/ScintillaCodePage.xaml.cs
+++ InkCode/ScintillaCodePage.xaml.cs
@@ -5,6 +5,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Microsoft.Windows.Storage.Pickers;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,6 +13,9 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Provider;
+using Windows.Storage.Streams;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +30,125 @@ namespace InkCode
         public ScintillaCodePage()
         {
             InitializeComponent();
+        }
+
+        private void Paste(SplitButton sender, SplitButtonClickEventArgs args)
+        {
+            editor.Editor.Paste();
+        }
+
+        private void Paste2(object sender, RoutedEventArgs e)
+        {
+            editor.Editor.Paste();
+        }
+
+        private void Copy(object sender, RoutedEventArgs e)
+        {
+            editor.Editor.Copy();
+        }
+
+        private void Cut(object sender, RoutedEventArgs e)
+        {
+            editor.Editor.Cut();
+        }
+
+        private void fontBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void fontSizeBox_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
+        {
+            if (editor != null)
+            {
+                editor.FontSize = fontSizeBox.Value;
+            }
+        }
+
+        private async void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                // Create the picker using the AppWindowId from the element
+                var picker = new FileOpenPicker(button.XamlRoot.ContentIslandEnvironment.AppWindowId)
+                {
+                    SuggestedStartLocation = PickerLocationId.DocumentsLibrary
+                };
+
+                // Add file type filters
+                picker.FileTypeFilter.Add(".txt");
+
+                // Show picker
+                PickFileResult result = await picker.PickSingleFileAsync();
+
+                if (result != null)
+                {
+                    // Open with StorageFile (needed for RichEditBox)
+                    StorageFile file = await StorageFile.GetFileFromPathAsync(result.Path);
+
+                    using IRandomAccessStream randAccStream =
+                        await file.OpenAsync(FileAccessMode.Read);
+
+                    // Load file into the RichEditBox
+                    using (StreamReader streamReader = new StreamReader(result.Path))
+                    {
+                        editor.Editor.AddText((long)randAccStream.Size, streamReader.ReadToEnd());
+                        switch (file.FileType)
+                        {
+                            case "cs":
+                                editor.HighlightingLanguage = "csharp";
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                // Create the picker with AppWindowId
+                var savePicker = new FileSavePicker(button.XamlRoot.ContentIslandEnvironment.AppWindowId)
+                {
+                    SuggestedStartLocation = PickerLocationId.DocumentsLibrary,
+                    SuggestedFileName = "Untitled"
+                };
+
+                // Dropdown of file types the user can save the file as
+                savePicker.FileTypeChoices.Add("Rich Text", new List<string>() { ".rtf" });
+
+                // Show picker
+                PickFileResult result = await savePicker.PickSaveFileAsync();
+
+                if (result != null)
+                {
+                    // Convert PickSaveFileResult to StorageFile
+                    StorageFile file = await StorageFile.GetFileFromPathAsync(result.Path);
+
+                    // Prevent updates to the remote version of the file until complete
+                    CachedFileManager.DeferUpdates(file);
+
+                    // Write content into the file
+                    using IRandomAccessStream randAccStream =
+                        await file.OpenAsync(FileAccessMode.ReadWrite);
+
+                    Windows.Storage.Streams.Buffer buffer = new Windows.Storage.Streams.Buffer((uint)editor.Editor.Length);
+                    editor.Editor.GetTextWriteBuffer(editor.Editor.Length, buffer);
+                    await randAccStream.WriteAsync(buffer);
+
+                    // Finalize file updates
+                    FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
+
+                    if (status != FileUpdateStatus.Complete)
+                    {
+                        var errorBox = new Windows.UI.Popups.MessageDialog(
+                            $"File {file.Name} couldn't be saved.");
+                        await errorBox.ShowAsync();
+                    }
+                }
+            }
         }
     }
 }
