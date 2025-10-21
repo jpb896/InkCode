77de3f5a728c9efc4dae91626b9da086be2efef1
jpb <jakub_baranski@outlook.com>
2025-10-21 19:26:24 +0100

    Code: Add more file extensions to open and save dialog boxes

--- InkCode/ScintillaCodePage.xaml.cs
+++ InkCode/ScintillaCodePage.xaml.cs
@@ -65,6 +65,15 @@ namespace InkCode
                 // Add file type filters
                 picker.FileTypeFilter.Add("*");
                 picker.FileTypeFilter.Add(".txt");
+                picker.FileTypeFilter.Add(".cs");
+                picker.FileTypeFilter.Add(".xaml");
+                picker.FileTypeFilter.Add(".xml");
+                picker.FileTypeFilter.Add(".cpp");
+                picker.FileTypeFilter.Add(".cxx");
+                picker.FileTypeFilter.Add(".html");
+                picker.FileTypeFilter.Add(".js");
+                picker.FileTypeFilter.Add(".yml");
+                picker.FileTypeFilter.Add(".json");
 
                 // Show picker
                 PickFileResult result = await picker.PickSingleFileAsync();
@@ -119,11 +128,12 @@ namespace InkCode
                             case ".rtf":
                                 ContentDialog rtf_dialog = new ContentDialog();
                                 rtf_dialog.Title = "Looking to edit an RTF file?";
-                                rtf_dialog.Content = "If you want to edit an RTF file quickly and seamlessly using WYSIWYG tools, use the RTF editing mode of the app, by opening a new tab having selected 'New rich text document' and opening the file from there.";
+                                rtf_dialog.Content = "If you want to edit an RTF file quickly and seamlessly using WYSIWYG tools, use the RTF editing mode of the app. To use it, open a new tab selecting 'New rich text document' from the new tab menu and opening the file from there.";
                                 rtf_dialog.XamlRoot = this.XamlRoot;
                                 rtf_dialog.IsPrimaryButtonEnabled = true;
                                 rtf_dialog.PrimaryButtonText = "Understood!";
                                 await rtf_dialog.ShowAsync();
+                                editor.HighlightingLanguage = "plaintext";
                                 break;
                             default:
                                 editor.HighlightingLanguage = "plaintext";
@@ -146,8 +156,15 @@ namespace InkCode
                 };
 
                 // Dropdown of file types the user can save the file as
-                savePicker.FileTypeChoices.Add("All files", new List<string>() { "" });
+                savePicker.FileTypeChoices.Add("All files", new List<string>() { "." });
                 savePicker.FileTypeChoices.Add("Plain text", new List<string>() { ".txt" });
+                savePicker.FileTypeChoices.Add("C#", new List<string>() { ".cs" });
+                savePicker.FileTypeChoices.Add("Extensible (Application) Markup Language", new List<string>() { ".xml", ".xaml" });
+                savePicker.FileTypeChoices.Add("C++", new List<string>() { ".cpp", ".cxx" });
+                savePicker.FileTypeChoices.Add("HyperText Markup Language", new List<string>() { ".html" });
+                savePicker.FileTypeChoices.Add("JavaScript", new List<string>() { ".js" });
+                savePicker.FileTypeChoices.Add("YAML", new List<string>() { ".yml" });
+                savePicker.FileTypeChoices.Add("JSON", new List<string>() { ".json" });
 
                 // Show picker
                 PickFileResult result = await savePicker.PickSaveFileAsync();
