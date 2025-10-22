144a0ec5d0a5b17581a2d94f87faee2087c33207
jpb <jakub_baranski@outlook.com>
2025-10-22 09:20:22 +0100

    Settings: Add build date display support to version information section

---
+++ InkCode/BuildDateAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace InkCode
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    internal class BuildDateAttribute : Attribute
+    {
+        public BuildDateAttribute(string value)
+        {
+            DateTime = DateTime.ParseExact(value, "yyyyMMddHHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None);
+        }
+
+        public DateTime DateTime { get; }
+    }
+}
