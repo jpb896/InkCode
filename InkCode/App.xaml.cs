567d931a458f75e9a57b6ea2719ee4a42bebd3cf
jpb <jpb44173@gmail.com>
2026-05-09 16:40:23 +0100

    Introduce localization

--- InkCode/App.xaml.cs
+++ InkCode/App.xaml.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.UI.Xaml;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+using WinUI3Localizer;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -25,10 +30,26 @@ namespace InkCode
         /// Invoked when the application is launched.
         /// </summary>
         /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        protected override async void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
             window = new MainWindow();
             window.Activate();
+            await InitializeLocalizer();
+        }
+
+        private async Task InitializeLocalizer()
+        {
+            // Initialize a "Strings" folder in the executables folder.
+            string stringsFolderPath = Path.Combine(AppContext.BaseDirectory, "Strings");
+            StorageFolder stringsFolder = await StorageFolder.GetFolderFromPathAsync(stringsFolderPath);
+
+            ILocalizer localizer = await new LocalizerBuilder()
+                .AddStringResourcesFolderForLanguageDictionaries(stringsFolderPath)
+                .SetOptions(options =>
+                {
+                    options.DefaultLanguage = "en-US";
+                })
+                .Build();
         }
     }
 }
